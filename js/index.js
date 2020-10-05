6fb56378fc4c51412d0968efee06e0b1a96bd641
Ramazan <ramazzan.ittiev@gmail.com>
2020-10-05 18:36:38 +0300

    Update 2020-10-05T15:36:34.577Z

--- js/index.js
+++ js/index.js
@@ -7,6 +7,34 @@ $(document).ready(function () {
   //   preloader: false,
   //   fixedContentPos: false,
   // });
+
+  let $menu = $('#menu');
+  let $logo = $('#logo');
+  let $logo2 = $('#logo2');
+  let $headerLink = $('.header__link');
+  let $headerSignUp = $('.header__signup');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 600 && $menu.hasClass('header')) {
+      $menu.fadeOut('fast', function () {
+        $menu.removeClass('header').addClass('header__fixed').fadeIn('fast');
+        $logo.addClass('header__logo-img-none');
+        $logo2.removeClass('header__logo-img-none');
+      });
+    } else if ($(this).scrollTop() <= 700 && $menu.hasClass('header__fixed')) {
+      $menu.fadeOut('fast', function () {
+        $menu.removeClass('header__fixed').addClass('header').fadeIn('fast');
+        $logo.removeClass('header__logo-img-none');
+        $logo2.addClass('header__logo-img-none');
+      });
+    } else if ($menu.hasClass('header__fixed')) {
+      $headerLink.css('color', 'black');
+      $headerSignUp.css('color', 'black');
+    } else if ($menu.hasClass('header')) {
+      $headerLink.css('color', '#d7d5d5');
+      $headerSignUp.css('color', '#d7d5d5');
+    }
+  });
 });
 
 let humburger = document.querySelector('.header__humburger');
